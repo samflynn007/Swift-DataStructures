f9827c83237299cd5a1f58932ca56a43db5a853d
Venkatesh nellore <51104553+samflynn007@users.noreply.github.com>
2023-10-02 17:48:51 +0530

    problem 2

--- Array/RemoveElemnt.playground/Contents.swift
+++ Array/RemoveElemnt.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Input: nums = [3,2,2,3], val = 3
+//Output: 2, nums = [2,2,_,_]
+
+func removeElement(_ arr: inout [Int], val: Int) -> Int {
+    var left = 0
+    for i in 0..<arr.count {
+        if arr[i] != val {
+            arr[left] = arr[i]
+            left += 1
+        }
+    }
+    return left
+}
+
+var nums = [3,2,2,3]
+var val = 3
+print(removeElement(&nums, val: val))
